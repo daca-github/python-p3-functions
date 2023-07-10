bee93b42bd98d95f5d05d0661a3a18188737084e
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-07-09 20:31:27 -0400

    Complete

--- lib/functions.py
+++ lib/functions.py
@@ -14,3 +14,18 @@ def add(num1, num2):
 
 def halve(number):
     pass
+
+def greet_programmer():
+    print("Hello, programmer!")
+
+def greet(name="Guido"):
+    print(f"Hello, {name}!")
+
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
+def add(num1, num2):
+    return num1+num2
+
+def halve(num1):
+    return num1 /2
